b16a56055a4495370466e71fcf5ed5431cd1724f
石川幹晴 <kansei.ishikawa@ishikawatsuyoshiharenoMacBook-Pro.local>
2018-02-03 19:35:26 +0900

    中継地点からファイルを引っ張ってくる

---
+++ upload-file-check.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#ディレクトリの数を確認
+count=`ssh rionerelaypoint "cd ~/share; ls -1 | wc -l"`
+for ((var = 1; var < $count + 1; var++))
+do
+  #一つ目のディレクトリから調べる
+  VAR1=`ssh rionerelaypoint "cd ~/share; ls -t | sed -n ${var}P"`
+  #ファイルサーバーに送るファイルの数を調べる
+  uploadcount=`ssh rionerelaypoint "cd ~/share/$VAR1; ls -1 | wc -l"`
+  #ファイルサーバーのどこに置くかを確認
+  dlname=`ssh rionerelaypoint "cd ~/share/$VAR1; cat .absolutePathOfFileServer"`
+  #ファイルの数だけループを回す
+  for ((i=1 ; i < ${uploadcount} + 1 ; i++))
+  do
+    #送るファイルまたはディレクトリの名前を取得
+    name=`ssh rionerelaypoint "cd ~/share/$VAR1; ls -t | sed -n ${i}P"`
+    math=`ssh rionerelaypoint "cd ~/share/$VAR1; test -d $name"`
+    if [ $math ] ; then
+      echo "ディレクトリ"
+      #ディレクトリを送る
+      #scp -i ~/.ssh/ri-oneFileServerRelayPoint.pem -r ec2-user@13.231.55.13 ~/share/${name} ${dlname}
+    elif [ !$math ] ; then
+      echo "ファイル"
+      #scp -i ~/.ssh/ri-oneFileServerRelayPoint.pem -f ec2-user@13.231.55.13 ~/share/${name} ${dlname}
+    fi
+  done
+done
