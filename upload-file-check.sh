465460b553deb9ddd9427ab2a85e08c7e14981b3
石川幹晴 <kansei.ishikawa@ishikawatsuyoshiharenoMacBook-Pro.local>
2018-02-04 15:32:53 +0900

    修正版

--- upload-file-check.sh
+++ upload-file-check.sh
@@ -15,15 +15,18 @@ do
   do
     #送るファイルまたはディレクトリの名前を取得
     name=`ssh rionerelaypoint "cd ~/share/$VAR1; ls -t | sed -n ${i}P"`
-    math=`ssh rionerelaypoint "cd ~/share/$VAR1; test -d $name"`
-    if [ $math ] ; then
-      echo "ディレクトリ"
+   # math=`ssh rionerelaypoint "cd ~/share/$VAR1; test -d $name ; echo $?"`
+   # echo $name
+   # echo math=$math
+   # if [ $math = "0" ] ; then
+    #  echo "ディレクトリ"
       #ディレクトリを送る
-      scp -i ~/.ssh/ri-oneFileServerRelayPoint.pem ec2-user@13.231.55.13:~/share/${VAR1}/${name} ${dlname}
-    elif [ !$math ] ; then
-      echo "ファイル"
-      scp -i ~/.ssh/ri-oneFileServerRelayPoint.pem ec2-user@13.231.55.13:~/share/${VAR1}/${name} ${dlname}
-      echo "成功"
-    fi
+      scp -i ~/.ssh/ri-oneFileServerRelayPoint.pem -r ec2-user@52.192.59.159:~/share/${VAR1}/${name} ${dlname}
+   # elif [ $math = "1" ] ; then
+     # echo "ファイル"
+     # scp -i ~/.ssh/ri-oneFileServerRelayPoint.pem ec2-user@13.231.55.13:~/share/${VAR1}/${name} ${dlname}
+     # echo "成功"
+   # fi
   done
 done
+ssh rionerelaypoint "cd ~/share; rm -r *"
